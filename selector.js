1e2b665df68bd8ee27b529be750b5a6d751260d8
Béla Varga <bva@financial.com>
2014-12-02 17:47:57 +0100

    Added checking for sibling position.

--- selector.js
+++ selector.js
@@ -2,70 +2,106 @@
     var result = document.getElementById('result');
     var domNode = document.getElementsByClassName('target')[0];
 
-    var uniquePath = null;
-    var path = [];
-    var node = null;
-    var foundUnique = false;
+    var UniqueSelector = {};
 
-    var traverseUpTree = function(domNode) {
-        if (!foundUnique) {
-            if (domNode.className && isClassUnique(domNode.className)) {
-                var className = getClassNames(domNode.classList);
-                node = '.' + className.replace(' ', '.');
-                path.push(node);
-                foundUnique = true;
+    UniqueSelector.get = function(domNode) {
+        var uniqueSelector = null;
+        var path = [];
+        var node = null;
+        var foundUnique = false;
+
+        var traverseUpTree = function(domNode) {
+            if (!foundUnique) {
+                if (domNode.className && isClassUnique(domNode.className)) {
+                    var className = getClassNames(domNode.classList);
+                    node = '.' + className.replace(' ', '.');
+                    path.push(node);
+                    foundUnique = true;
+                } else {
+                    node = domNode.localName;
+                    var position = getSiblingPosition(domNode);
+                    if (position !== 0) { // 0 direct child
+                        node = node + ':nth-of-type(' + position + ')';
+                    }
+                    path.push(node);
+                    path.push('>');
+                }
+            }
+            if (domNode.nodeName !== 'BODY' && domNode.className.indexOf('moduleId_') === -1) {
+                traverseUpTree(domNode.parentNode);
             } else {
-                node = domNode.localName;
-                var position = getSiblingPosition(domNode);
-                if(position !== 1) {
-                    node = node + ':nth-of-type(' + position + ')';
-                }
-                path.push(node);
-                path.push('>');
-            }
-        }
-        if (domNode.nodeName !== 'BODY') {
-            traverseUpTree(domNode.parentNode);
-        } else {
-            path.push(domNode.localName);
-        }
-    };
-
-    var getSiblingPosition = function(domNode) {
-        var position = 1;
-
-        var countNodes = function(elm) {
-            if(elm.previousSibling !== null) {
-                if(elm.previousSibling.localName === domNode.localName) {
-                    position = position + 1;
-                }
-                countNodes(elm.previousSibling);
+                path.push(domNode.localName);
             }
         };
 
-        countNodes(domNode);
-
-        return position;
-    };
-
-    var isClassUnique = function(className) {
-        return document.getElementsByClassName(className).length === 1;
-    };
-
-    var getClassNames = function(classNameList) {
-        var classNames = '';
-        for (var i = 0; i < classNameList.length; i++) {
-            classNames += classNameList[i];
-            if (i < classNameList.length - 1) {
-                classNames += ' ';
+        var getModuleIdClass = function(domNode) {
+            var moduleIdClass = '';
+            var classList = domNode.classList;
+            for (var i = 0; i < classList.length; i++) {
+                if (classList[i].indexOf('moduleId_') !== -1) {
+                    moduleIdClass = '.' + classList[i];
+                }
             }
+            return moduleIdClass;
         };
-        return classNames;
-    }
 
-    traverseUpTree(domNode);
+        // 0 if direct child
+        // 1 if first child
+        var getSiblingPosition = function(domNode) {
+            var position = 0;
 
-    uniquePath = path.reverse().join(' ');
+            var countPrevSiblings = function(elm) {
+                if (elm.previousElementSibling !== null) {
+                    if (position === 0) {
+                        position = 1;
+                    }
+                    if (elm.previousElementSibling.localName === domNode.localName) {
+                        position = position + 1;
+                    }
+                    countPrevSiblings(elm.previousElementSibling);
+                }
+            };
+
+            var countNextSiblings = function(elm) {
+                if (elm.nextElementSibling !== null) {
+                    if (elm.nextElementSibling.localName === domNode.localName) {
+                        if (position === 0) {
+                            position = 1;
+                        }
+                    }
+                    countNextSiblings(elm.nextElementSibling);
+                }
+            };
+
+            countPrevSiblings(domNode);
+            countNextSiblings(domNode);
+
+            return position;
+        };
+
+        var isClassUnique = function(className) {
+            return document.getElementsByClassName(className).length === 1;
+        };
+
+        var getClassNames = function(classNameList) {
+            var classNames = '';
+            for (var i = 0; i < classNameList.length; i++) {
+                classNames += classNameList[i];
+                if (i < classNameList.length - 1) {
+                    classNames += ' ';
+                }
+            }
+            return classNames;
+        };
+
+        traverseUpTree(domNode);
+
+        uniqueSelector = path.reverse().join(' ');
+
+        return uniqueSelector;
+    };
+
+    var uniquePath = UniqueSelector.get(domNode);
 
     result.innerHTML = uniquePath;
 
